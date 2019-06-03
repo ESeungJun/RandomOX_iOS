6837a702887e2363bb23151dc22a0554bbb3bcce
이승준 <6951004@gmail.com>
2019-06-03 15:31:17 +0900

    feat : 최초 소스 푸쉬

---
+++ randomox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomox
+//
+//  Created by 이승준 on 03/06/2019.
+//  Copyright © 2019 Eseungjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
