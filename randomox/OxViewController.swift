47040bb8c5c9aa07a0f269e1c23214178991e422
이승준 <6951004@gmail.com>
2019-06-10 14:50:32 +0900

    feat : ox 퀴즈 화면 추가 및 화면이동 추가

---
+++ randomox/OxViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OxViewController.swift
+//  randomox
+//
+//  Created by 이승준 on 03/06/2019.
+//  Copyright © 2019 Eseungjun. All rights reserved.
+//
+
+import UIKit
+
+class OxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- randomox/ViewController.swift
+++ randomox/ViewController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func testBtnAction(_ sender: Any) {
+        let oxVC = OxViewController()
+        self.navigationController?.pushViewController(oxVC, animated: true)
+        
+    }
+    
 }
+
 
